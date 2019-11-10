e666b51dd46b43463bc09d4a73e01806c6e067ba
MohammadMRD <mohammad.moradi9375@gamil.com>
2019-11-10 14:31:27 +0330

    Add `Header` to layout

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,12 +1,17 @@
+// Material-ui core
 import Container from '@material-ui/core/Container';
+
+// Main core
 import Header from '@components/header';
 
 const Layout = ({ children }) => {
   return (
-    <Container>
+    <>
       <Header />
-      {children}
-    </Container>
+      <Container>
+        {children}
+      </Container>
+    </>
   )
 }
 
