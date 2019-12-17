4b063aead6fe052222bafaea74970bd467477cb2
MohammadMRD <mohammad.moradi9375@gamil.com>
2019-12-17 19:43:30 +0330

    Define utility style for shapes

---
+++ src/utils/styles/shape.js
@@ -0,0 +1,19 @@
+// Styled-tools
+import { withProp, prop } from 'styled-tools';
+
+// Styled-components
+import { css } from 'styled-components';
+
+
+// Props
+const props = {
+  borderRadius: 'borderRadius',
+  shape: prop('theme.shape', {})
+}
+
+const shape = withProp(
+  Object.values(props),
+  (borderRadius, shape) => borderRadius && css`border-radius: ${shape[borderRadius] || borderRadius}px;`
+);
+
+export default shape;
