e3618a79c4bbdcd640c255c14c284d44c09fe0ca
MohammadMRD <mohammad.moradi9375@gamil.com>
2019-12-17 23:01:38 +0330

    Fix nowrap prop in typography

--- src/utils/styles/typography.js
+++ src/utils/styles/typography.js
@@ -28,7 +28,7 @@ const typography = withProp(
       font-style: ${variantInfo.fontStyle};
       text-align: ${align};
       display: ${display};
-      ${noWrap && css`white-space: inherit;`}
+      ${noWrap && css`white-space: nowrap;`}
     `
   }
 );
