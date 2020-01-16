b2cf07372d0f081d076529ec2b9d8be408a4ee5d
MohammadMRD <mohammad.moradi9375@gamil.com>
2020-01-17 02:28:48 +0330

    Create typography file, Create overrides file for override global font-face

--- src/utils/theme/index.js
+++ src/utils/theme/index.js
@@ -2,17 +2,13 @@
 import { createMuiTheme } from '@material-ui/core';
 
 // Main core
-import { TYPOGRAPHY, GLOBAL } from '@utils/constants';
 import palette from './palette';
+import typography from './typography';
+import overrides from './override-material-styles';
 
 // ---------------------------------------------
 
 const shape = { borderRadius: 98,  default: 8, square: 0, rounded: 98 };
-const overrides = {
-  MuiCssBaseline: { '@global': GLOBAL } 
-};
-
-const typography = TYPOGRAPHY;
 
 const theme = createMuiTheme({ palette, shape, overrides, typography });
 
