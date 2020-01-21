d1ae667790dff59f6ffdcc051abf245514500be9
Mohammad Moradi <32971123+MohammadMRD@users.noreply.github.com>
2020-01-21 20:14:10 +0330

    Add Home page (#13)
    * Show list of products in home page
    * Add `pages-style`  folder in absolute path configs
    * Install `Axios` library
    * Add font in the theme
    * Update package-lock
    * Modify and refactor home page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,79 @@
-export default () => <div>Home page</div>
+// React core
+import { useState, useEffect } from 'react';
+
+// Axios 
+import Axios from 'axios';
+
+// Material-ui core
+import Grid from '@material-ui/core/Grid';
+
+//Main core
+import Flex from '@components/common/flex';
+import Layout from '@components/layout';
+
+// Styles
+import * as Styles from '@pages-style/index-style';
+
+// Custom hooks
+const useProducts = () => {
+  const [products, setProducts] = useState([]);
+  
+  const getAllProducts = async () => {
+    try {
+      // TODO: Change url with original api
+      const allProducts = await Axios.get('https://my-json-server.typicode.com/MohammadMRD/Fake-data/products');
+      setProducts(allProducts.data);
+    } catch (ex) {
+      // TODO: Show Error message to user
+      console.log(ex);
+    }
+  }
+
+  useEffect(() => {
+    getAllProducts();
+  }, [])
+
+  // TODO: Add `Loading` and `Error` state
+  return products;
+}
+
+// Components 
+const Product = ({ info }) => {
+
+  return (
+    <Styles.ProductWrapper disablePadding as={Flex} flexDirection='column'>
+      <Styles.ProductImage src={info.imgUrl} alt={info.name}/>
+      <Styles.ProductText variant='h6' align='center'>{info.name}</Styles.ProductText>
+      <Styles.ProductText variant='h6' as='span' color='grey.500'>$&nbsp;{info.price}</Styles.ProductText>
+    </Styles.ProductWrapper>
+  )
+}
+
+const Products = () => {
+  const products = useProducts();
+
+  return (
+    <Grid container spacing={4}>
+      {
+        products.map(product => (
+          <Grid item md={3} key={product.id}>
+            <Product info={product} />
+          </Grid>
+        ))
+      }
+    </Grid>
+  )
+}
+
+const Home = () => {  
+  return (
+    <>
+      <Styles.ProductsTitle variant='h3'>All Products</Styles.ProductsTitle>
+      <Products />
+    </>
+  )
+}
+
+Home.Layout = Layout;
+
+export default Home;
