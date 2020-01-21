080bfa9702b8326b32c139e0ceb4120a19c5966c
Mohammad Moradi <32971123+MohammadMRD@users.noreply.github.com>
2020-01-21 20:21:10 +0330

    Revert "Move colors defination to `constants` file, Add `Success` and `Warning` color"
    This reverts commit 273d6173515acdb3bd3a269e2f7b63343ad1132e.

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,25 +1,36 @@
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core";
 import createPalette from "@material-ui/core/styles/createPalette";
-import {
-	PRIMARY_COLOR,
-	SECONDARY_COLOR,
-	ERROR_COLOR,
-	GREY_COLOR,
-  SUCCESS_COLOR,
-  WARNING_COLOR
-} from './constants';
 
 
 const palette = createPalette({
-  primary: PRIMARY_COLOR,
-  secondary: SECONDARY_COLOR,
-  error: ERROR_COLOR,
-  success: SUCCESS_COLOR,
-  warning: WARNING_COLOR,
-  grey: GREY_COLOR
+  primary: {
+    light: '#4398bb',
+    main: '#147FAB',
+    dark: '#0e5877',
+  },
+  secondary: {
+    light: '#ff7060',
+    main: '#db3c35',
+    dark: '#a2000d',
+  },
+  error: {
+    light: '#ff9893',
+    main: '#ff6565',
+    dark: '#c6303b'
+  },
+  grey: {
+    '100': '#FAFAFA',
+    '200': '#E5E5E5',
+    '300': '#CCCCCC',
+    '400': '#999999',
+    '500': '#666666',
+    '600': '#333333'
+  }
 });
 
-const shape = { borderRadius: 98 };
-
-const theme = createMuiTheme({ palette, shape });
-export default responsiveFontSizes(theme);
+export default responsiveFontSizes(createMuiTheme({
+  palette,
+  shape: {
+    borderRadius: 98
+  }
+}));
