7480975bffb937b44a07db86ad96d78162b78339
Mohammad Moradi <32971123+MohammadMRD@users.noreply.github.com>
2020-01-29 19:54:06 +0330

    Feature/modify home page (#29)
    * Install dotenv and Change api call for list of products
    
    * Add loading and error states for list of products

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,77 @@
-// React core
-import { useState, useEffect } from 'react';
-
-// Axios 
-import Axios from 'axios';
-
 // Material-ui core
 import Grid from '@material-ui/core/Grid';
 
+// Material-ui lab
+import Alert from '@material-ui/lab/Alert';
+
 //Main core
-import Flex from '@components/common/flex';
 import Layout from '@components/layout';
+import product from '@services/api/product';
 
 // Styles
 import * as Styles from '@pages-style/index-style';
 
-// Custom hooks
-const useProducts = () => {
-  const [products, setProducts] = useState([]);
-  
-  const getAllProducts = async () => {
-    try {
-      // TODO: Change url with original api
-      const allProducts = await Axios.get('https://my-json-server.typicode.com/MohammadMRD/Fake-data/products');
-      setProducts(allProducts.data);
-    } catch (ex) {
-      // TODO: Show Error message to user
-      console.log(ex);
-    }
-  }
-
-  useEffect(() => {
-    getAllProducts();
-  }, [])
-
-  // TODO: Add `Loading` and `Error` state
-  return products;
-}
 
 // Components 
 const Product = ({ info }) => {
 
   return (
-    <Styles.ProductWrapper disablePadding as={Flex} flexDirection='column'>
+    <Styles.ProductWrapper disablePadding>
       <Styles.ProductImage src={info.imgUrl} alt={info.name}/>
+      
       <Styles.ProductText variant='h6' align='center'>{info.name}</Styles.ProductText>
-      <Styles.ProductText variant='h6' as='span' color='grey.500'>$&nbsp;{info.price}</Styles.ProductText>
+      
+      <Styles.ProductText variant='h6' as='span' color='grey.500'>
+        $&nbsp;{info.price}
+      </Styles.ProductText>
     </Styles.ProductWrapper>
   )
 }
 
-const Products = () => {
-  const products = useProducts();
+const ProductsLoading = () => {
+  const tempArray = Array(4).fill();
 
+  return tempArray.map((item, index) => (
+    <Grid item md={3}>
+      <Styles.ProductWrapper disablePadding key={index}>
+        <Styles.Skelton variant='rect' animation='wave' height={225} width='100%' />
+        <Styles.Skelton variant='text' animation='wave' width='80%' my={1} mx='auto' />
+        <Styles.Skelton variant='text' animation='wave' width='30%' mb={1} mx='auto' />
+      </Styles.ProductWrapper>
+    </Grid>
+  ));
+}
+
+const ProductError = ({ error }) => {
+  const UNKNOWN_ERROR = 'Unknown Error!';
+  const err = {
+    object: (error && error.message) || UNKNOWN_ERROR,
+    string: error
+  }
+
+  return (
+    <Alert severity='error' variant='filled'>
+      {err[typeof error] || UNKNOWN_ERROR}
+    </Alert>
+  )
+}
+
+const Products = () => {
+  const [products, error, loading] = product.getAll();
+  
   return (
     <Grid container spacing={4}>
       {
-        products.map(product => (
+        error ? <ProductError error={error} />:
+        loading ? <ProductsLoading />:
+        products && products.map(product => (
           <Grid item md={3} key={product.id}>
             <Product info={product} />
           </Grid>
         ))
       }
     </Grid>
-  )
+  );
 }
 
 const Home = () => {  
