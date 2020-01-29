7374297710b4693d3443839a6888edf1aa851f37
Mohammad Moradi <mohammad.moradi9375@gmail.com>
2020-01-29 19:43:08 +0330

    Add loading and error states for list of products

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 // Material-ui core
 import Grid from '@material-ui/core/Grid';
 
-//Main core
-import Flex from '@components/common/flex';
-import Layout from '@components/layout';
+// Material-ui lab
+import Alert from '@material-ui/lab/Alert';
 
+//Main core
+import Layout from '@components/layout';
 import product from '@services/api/product';
 
 // Styles
@@ -15,21 +16,54 @@ import * as Styles from '@pages-style/index-style';
 const Product = ({ info }) => {
 
   return (
-    <Styles.ProductWrapper disablePadding as={Flex} flexDirection='column'>
+    <Styles.ProductWrapper disablePadding>
       <Styles.ProductImage src={info.imgUrl} alt={info.name}/>
+      
       <Styles.ProductText variant='h6' align='center'>{info.name}</Styles.ProductText>
-      <Styles.ProductText variant='h6' as='span' color='grey.500'>$&nbsp;{info.price}</Styles.ProductText>
+      
+      <Styles.ProductText variant='h6' as='span' color='grey.500'>
+        $&nbsp;{info.price}
+      </Styles.ProductText>
     </Styles.ProductWrapper>
   )
 }
 
-const Products = () => {
-  // TODO: Design error and loading state
-  const [products, error, loading] = product.getAll();
+const ProductsLoading = () => {
+  const tempArray = Array(4).fill();
 
+  return tempArray.map((item, index) => (
+    <Grid item md={3}>
+      <Styles.ProductWrapper disablePadding key={index}>
+        <Styles.Skelton variant='rect' animation='wave' height={225} width='100%' />
+        <Styles.Skelton variant='text' animation='wave' width='80%' my={1} mx='auto' />
+        <Styles.Skelton variant='text' animation='wave' width='30%' mb={1} mx='auto' />
+      </Styles.ProductWrapper>
+    </Grid>
+  ));
+}
+
+const ProductError = ({ error }) => {
+  const UNKNOWN_ERROR = 'Unknown Error!';
+  const err = {
+    object: (error && error.message) || UNKNOWN_ERROR,
+    string: error
+  }
+
+  return (
+    <Alert severity='error' variant='filled'>
+      {err[typeof error] || UNKNOWN_ERROR}
+    </Alert>
+  )
+}
+
+const Products = () => {
+  const [products, error, loading] = product.getAll();
+  
   return (
     <Grid container spacing={4}>
       {
+        error ? <ProductError error={error} />:
+        loading ? <ProductsLoading />:
         products && products.map(product => (
           <Grid item md={3} key={product.id}>
             <Product info={product} />
@@ -37,7 +71,7 @@ const Products = () => {
         ))
       }
     </Grid>
-  )
+  );
 }
 
 const Home = () => {  
